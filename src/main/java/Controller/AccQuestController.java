6b0eee660abb992c1f61b6efba1c55158f289cd9
Thomas <kaisser.thomas@gmail.com>
2023-04-10 14:51:47 +0300

    second commit

--- src/main/java/Controller/AccQuestController.java
+++ src/main/java/Controller/AccQuestController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import javax.management.StringValueExp;
 import java.io.IOException;
 
 
@@ -24,6 +25,8 @@ public class AccQuestController {
     private TriviaQuest quest;
 
     private int tries;
+
+    private int hints;
 
     @FXML
     Label questionLabel;
@@ -45,7 +48,7 @@ public class AccQuestController {
     public void initData(){
         questionLabel.setText(quest.getQuestion());
         tries=3;
-
+        hints=0;
 
     }
 
@@ -109,5 +112,28 @@ public class AccQuestController {
         questsStage.show();
     }
 
+    public void hintHandler(){
+        if(hints ==0){
+            answerTextField.clear();
+            answerTextField.setText(quest.getAnswer().charAt(0)+"");
+            hints++;
+        }
+        else if(hints==1 && quest.getAnswer().length()>2){
+            answerTextField.clear();
+            answerTextField.setText(quest.getAnswer().charAt(0)+""+quest.getAnswer().charAt(1)+"");
+            hints++;
+        }
+        else if(hints==2 && quest.getAnswer().length()>3){
+            answerTextField.clear();
+            answerTextField.setText(quest.getAnswer().charAt(0)+""+quest.getAnswer().charAt(1)+""+quest.getAnswer().charAt(2));
+            hints++;
+        }else{
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Info");
+        alert.setHeaderText("Out of hints");
+        alert.setContentText("You have no more hints");
+        alert.showAndWait();
+        }
+    }
 
 }
